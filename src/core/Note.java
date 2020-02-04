10ba4476cefe6a708b9c8b43bc108dbeb12545e7
Matthew Cech <contact@matthewcech.com>
2020-02-03 22:19:14 -0800

    Added file write logging

--- src/core/Note.java
+++ src/core/Note.java
@@ -1,21 +1,55 @@
 package core;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Note
 {
+	private static Integer entryCount = 0;
+	
+	// Intentionally synchronize the whole thing so we don't have write issues.
+	private static void WriteOut(String toAppend)
+	{
+			try
+			{
+				FileWriter fileWriter = new FileWriter("log.txt", true);
+				PrintWriter printWriter = new PrintWriter(fileWriter);
+				
+				synchronized(entryCount)
+				{
+					printWriter.println("[" + String.format("%09d", entryCount) + "]" + toAppend); 
+					entryCount += 1;
+					printWriter.close();
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+	}
+	
+	private static void Print(String str)
+	{
+		System.out.println(str);
+		WriteOut(str);
+	}
+	
 	public static void Log(Object toWrite)
 	{
-		System.out.println("[Log] " + toWrite.toString());
+		String msg = "[Log] " + toWrite.toString();
+		Print(msg);
 	}
 	
 	public static void Warn(Object toWrite)
 	{
-		System.out.println("[Warning] " + toWrite.toString());
+		Print("[Warning] " + toWrite.toString());
 	}
 	
 	public static void Error(Object toWrite)
 	{
 		String asStr = toWrite.toString();
-		System.out.println("[ERROR] " + asStr);
+		Print("[ERROR] " + asStr);
 		System.err.println("[ERROR] " + asStr);
 	}
 }
